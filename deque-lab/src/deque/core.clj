27ba1a7b729b7714b38038a95b74a4572db7b24d
David <dghiurco@hawk.iit.edu>
2015-03-10 09:52:25 +1100

    Finished coding core.clj and started tests

--- deque-lab/src/deque/core.clj
+++ deque-lab/src/deque/core.clj
@@ -2,7 +2,7 @@
 
 (defrecord Deque [front back size])
 
-;; # Your Work
+;; David Ghiurco's deque lab
 
 (defn make-deque
   "Create an empty deque."
@@ -21,43 +21,56 @@
     (Deque. (cons elt front) back (inc size))))
 
 (defn push-back
-  "Adds an element to the back fo the deque."
+  "Adds an element to the back of the deque."
   [dq elt]
-  nil
- )
+  (let [{:keys [front back size]} dq]
+    (Deque. front (cons elt back) (inc size)))
+)
 
 (defn flip-front
   "Flip the back list to the front list, if necessary."
   [dq]
-  nil
+  (let [{:keys [front back size]} dq]
+    (cond (nil? front)  (Deque. (reverse back) nil size)
+          :else dq))
 )
 
 (defn flip-back
   "Flip the front list to the back list, if necessary."
   [dq]
-  nil
+  (let [{:keys [front back size]} dq]
+    (cond (nil? back)  (Deque. nil (reverse front) size)
+          :else dq))
 )
 
 (defn front
   "Return the front element of the deque.  May cause a flip."
   [dq]
-  nil
+  (let [{:keys [front back size]} dq]
+    (first (:front (flip-front dq))))  ;; flip-front checks if it should actually flip
 )
 
 (defn back
-  "Return the back element of the deque.  May cause a flip."
+  "return the back element of the deque.  may cause a flip."
   [dq]
-  nil
+  (let [{:keys [front back size]} dq]
+    (first (:back (flip-back dq)))) ;; flip-back checks if it should actually flip
 )
+
 
 (defn pop-front
-  "Pops/dequeues an element from the front of the deque."
+  "pops/dequeues an element from the front of the deque."
   [dq]
-  nil
+  (let [{:keys [front back size]} dq]
+    (cond (nil? front)  (Deque. (rest (:front (flip-front dq))) nil (dec size))
+    :else (Deque. (rest front) back (dec size))))
 )
 
+
 (defn pop-back
-  "Pops/dequeues an element from the back of the deque."
+  "pops/dequeues an element from the back of the deque."
   [dq]
-  nil
+  (let [{:keys [front back size]} dq]
+    (cond (nil? back) (Deque. nil (rest (:back (flip-back dq))) (dec size))
+    :else (Deque. front (rest back) (dec size))))
 )
--- deque-lab/test/deque/t_core.clj
+++ deque-lab/test/deque/t_core.clj
@@ -3,6 +3,14 @@
   (:use [deque.core])
   (:import [deque.core Deque] ))
 
-(facts "about this lab"
-  (fact "the student never started it."
-        (+ 1 2)  => 12))
+(facts "about make-deque"
+       (fact "it creates an empty deque"
+            (:size (make-deque)) => 0
+(facts "about deque-size"
+       (fact "it returns the size of a deque"
+             (let [(Deque. '(1 2 3) '(6 5 4) 6) dq]
+                 (deque-size dq) => (:size dq))))
+(facts "about push-front"
+       (fact "it adds an element to the front of the deque"
+             (let [(Deque. '(1 2 3) '(6 5 4) 6) dq]
+                  (:front (push-front dq 7)) => '(7 1 2 3))))
